6bae76b13132788883fbf33fdcb778f17d5ca385
Emperor-1231 <abdoakojdad@gmail.com>
2024-12-27 18:00:04 +0100

    build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,23 @@
 plugins {
-    id(Plugins.kotlinter.name) version Plugins.kotlinter.version
-    id(Plugins.gradleVersions.name) version Plugins.gradleVersions.version
-    id(Plugins.jetbrainsKotlin) version AndroidVersions.kotlin apply false
+    id("org.jlleitschuh.gradle.ktlint") version "11.0.0" apply false
+    id("com.github.ben-manes.versions") version "0.42.0" apply false
+    id("org.jetbrains.kotlin.jvm") version "1.8.10" apply false
 }
+
 allprojects {
     repositories {
         google()
         mavenCentral()
-        maven { setUrl("https://jitpack.io") }
-        maven { setUrl("https://plugins.gradle.org/m2/") }
+        maven { url = uri("https://jitpack.io") }
+        maven { url = uri("https://plugins.gradle.org/m2/") }
     }
 }
 
 subprojects {
-    apply(plugin = Plugins.kotlinter.name)
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
 
     kotlinter {
         experimentalRules = true
-
-        // لا يعمل بشكل جيد مع أندرويد ستوديو
         disabledRules = arrayOf("experimental:argument-list-wrapping")
     }
 }
@@ -27,9 +26,9 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:8.1.2")
         classpath("com.google.gms:google-services:4.3.15")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${AndroidVersions.kotlin}")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.10")
         classpath("com.google.android.gms:oss-licenses-plugin:0.10.6")
-        classpath("org.jetbrains.kotlin:kotlin-serialization:${AndroidVersions.kotlin}")
+        classpath("org.jetbrains.kotlin:kotlin-serialization:1.8.10")
         classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.4")
     }
     repositories {
@@ -43,7 +42,6 @@ tasks.named("dependencyUpdates", com.github.benmanes.gradle.versions.updates.Dep
     rejectVersionIf {
         isNonStable(candidate.version)
     }
-    // معلمات اختيارية
     checkForGradleUpdate = true
     outputFormatter = "json"
     outputDir = "build/dependencyUpdates"
