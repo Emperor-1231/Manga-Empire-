027eeef621b5877b2b5852ded0cc50adb5450516
Emperor-1231 <abdoakojdad@gmail.com>
2024-12-28 12:17:57 +0100

    build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,23 +1,24 @@
 plugins {
-    id("org.jlleitschuh.gradle.ktlint") version "11.0.0" apply false
-    id("com.github.ben-manes.versions") version "0.42.0" apply false
-    id("org.jetbrains.kotlin.jvm") version "1.8.10" apply false
+    id(Plugins.kotlinter.name) version Plugins.kotlinter.version
+    id(Plugins.gradleVersions.name) version Plugins.gradleVersions.version
+    id(Plugins.jetbrainsKotlin) version AndroidVersions.kotlin apply false
 }
-
 allprojects {
     repositories {
         google()
         mavenCentral()
-        maven { url = uri("https://jitpack.io") }
-        maven { url = uri("https://plugins.gradle.org/m2/") }
+        maven { setUrl("https://jitpack.io") }
+        maven { setUrl("https://plugins.gradle.org/m2/") }
     }
 }
 
 subprojects {
-    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+    apply(plugin = Plugins.kotlinter.name)
 
     kotlinter {
         experimentalRules = true
+
+        // Doesn't play well with Android Studio
         disabledRules = arrayOf("experimental:argument-list-wrapping")
     }
 }
@@ -26,9 +27,9 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:8.1.2")
         classpath("com.google.gms:google-services:4.3.15")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.10")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${AndroidVersions.kotlin}")
         classpath("com.google.android.gms:oss-licenses-plugin:0.10.6")
-        classpath("org.jetbrains.kotlin:kotlin-serialization:1.8.10")
+        classpath("org.jetbrains.kotlin:kotlin-serialization:${AndroidVersions.kotlin}")
         classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.4")
     }
     repositories {
@@ -42,6 +43,7 @@ tasks.named("dependencyUpdates", com.github.benmanes.gradle.versions.updates.Dep
     rejectVersionIf {
         isNonStable(candidate.version)
     }
+    // optional parameters
     checkForGradleUpdate = true
     outputFormatter = "json"
     outputDir = "build/dependencyUpdates"
